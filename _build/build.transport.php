c5ddce83306a5deb86d6fcd337b701177967a337
Bezumkin <bezumkin@yandex.ru>
2012-10-26 22:19:44 +0700

    Version 1.3.0

--- _build/build.transport.php
+++ _build/build.transport.php
@@ -14,7 +14,7 @@ set_time_limit(0);
 /* define package */
 define('PKG_NAME','Loginza');
 define('PKG_NAME_LOWER',strtolower(PKG_NAME));
-define('PKG_VERSION','1.2.3');
+define('PKG_VERSION','1.3.0');
 define('PKG_RELEASE','pl');
 
 /* define sources */
